2cd2a4ab75382c9e9ab4d1d5c9c58ffaad6ec610
AlineMolonCW <aline@cwsoftwares.com.br>
2017-08-10 13:47:16 -0300

    novas tarefas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
-import { ContatoComponent } from './contato/contato.component';
 import { NossaVisaoComponent } from './nossa-visao/nossa-visao.component';
-import { RouterModule } from '@angular/router';
+import { ContatoModule } from './contato/contato.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuemSomosComponent,
-    ContatoComponent,
     NossaVisaoComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule,
+    HttpModule,
+    FormsModule,
+    ContatoModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
